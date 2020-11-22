4997da1b6fa7d8c07f693ed16a0d814d1557666c
csk <sai2k1c@gmail.com>
2020-11-22 06:30:48 -0800

    initial

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ app = Flask (__name__)
 imgfolder = os.path.join('static','img')
 app.config['UPLOAD_FOLDER'] = imgfolder
 
-css = Bundle ('css/admin.css',output='gen/main.css')
+css = Bundle ('css/admin.css','css/home.css','css/login.css','css/payment.css',output='gen/main.css')
 assets = Environment(app)
 assets.register('main_css',css)
 
--- app/admin.py
+++ app/admin.py
@@ -4,5 +4,5 @@ from flask import render_template
 @app.route('/',methods=['GET','POST'])
 def add():
 
-    return render_template('base/new.html')
+    return render_template('base/register.html')
 
