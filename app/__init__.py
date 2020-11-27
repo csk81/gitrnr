a67d5f1c2e53cb89de53f3cf1e20d31a4bfdef7b
csk <sai2k1c@gmail.com>
2020-11-26 23:58:08 -0800

    lots of changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,34 @@
-from flask import Flask
+from flask import Flask,current_app
 from flask_assets import Bundle,Environment
-from flask_mysqldb import MySQL
 import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
 
 app = Flask (__name__)
 
-imgfolder = os.path.join('static','img')
+imgfolder = os.path.join('static','image')
 app.config['UPLOAD_FOLDER'] = imgfolder
 
-css = Bundle ('css/admin.css','css/home.css','css/login.css','css/payment.css',output='gen/main.css')
+css = Bundle ('css/admin.css','css/home.css','css/login.css','css/payment.css','css/explore.css','css/cars.css',output='gen/main.css')
 assets = Environment(app)
 assets.register('main_css',css)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root123'
-app.config['MYSQL_DB'] = 'demo'
+app.config['MYSQL_DB'] = 'cars'
 mysql = MySQL(app)
+
+
+
+def save_images(photo):
+    name = os.path.splitext(photo.filename)
+    print(os.path.splitext(photo.filename))
+    if name[1] in ".jpg,.png,.jpeg":
+        photo_name = name[0] + name[1]
+    file_path = os.path.join(current_app.root_path, 'static/image/cars',photo_name)
+    photo.save(file_path)
+
+    return photo_name
 
 from app import admin,sign
--- app/admin.py
+++ app/admin.py
@@ -1,20 +1,14 @@
-from app import app,mysql
+from app import app,mysql,save_images
 from flask import render_template, redirect , url_for ,current_app, flash , request
 import os
 import secrets
-def save_images(photo):
-    name = os.path.splitext(photo.filename)
-    print(os.path.splitext(photo.filename))
-    if name[1] in ".jpg,.png,.jpeg":
-        photo_name = name[0] + name[1]
-    file_path = os.path.join(current_app.root_path, 'static/image',photo_name)
-    photo.save(file_path)
-
-    return photo_name
 
 
 
-@app.route('/',methods=['GET','POST'])
+
+
+
+@app.route('/add',methods=['GET','POST'])
 def add():
     if request.method == "POST":
         carmodel  = request.form.get('carmodel')
@@ -23,8 +17,82 @@ def add():
         cost = request.form.get('cost')
         carpic = save_images(request.files.get ('carpic'))
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO admin(carmodel,fueltype,transmission,carpic) VALUES(%s,%s,%s,%s)",(carmodel,fueltype,transmission,carpic))
+        cur.execute("INSERT INTO car(carmodel,fueltype,transmission,cost,carpic) VALUES(%s,%s,%s,%s,%s)",(carmodel,fueltype,transmission,cost,carpic))
         mysql.connection.commit()
         cur.close()
+
     return render_template('base/add.html')
 
+@app.route('/d',methods=['GET','POST'])
+def display():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM admin")
+    a = cur.fetchall()
+    mysql.connection.commit()
+    cur.close()
+    for i in a:
+        filename = i[3]
+    with open(filename,'wb') as f:
+        x = f.write(filename)
+    return render_template('base/display.html',pic = x)
+
+
+@app.route('/login')
+def login():
+    bg_img = os.path.join (app.config['UPLOAD_FOLDER'],'bg.png')
+    return render_template('base/login.html', img1 = bg_img)
+
+@app.route('/')
+def home():
+    
+    return render_template('base/home.html')
+
+
+@app.route('/register')
+def reg():
+    bg_img = os.path.join (app.config['UPLOAD_FOLDER'],'bg.png')
+    return render_template('base/register.html', img1 = bg_img)
+
+@app.route('/admin')
+def admin():
+    bg_img = os.path.join (app.config['UPLOAD_FOLDER'],'bg.png')
+    return render_template('base/admin.html', img1 = bg_img)
+
+@app.route('/payment')
+def payment():
+    bg_img = os.path.join (app.config['UPLOAD_FOLDER'],'bg.png')
+    return render_template('base/admin.html', img1 = bg_img)
+
+@app.route('/explore')
+def explore():
+    first = os.path.join (app.config['UPLOAD_FOLDER'],'bg.png')
+    sec = os.path.join (app.config['UPLOAD_FOLDER'],'cars/i20.jpg')
+    tri = os.path.join (app.config['UPLOAD_FOLDER'],'cars/maruti.jpg')
+    return render_template('base/explore.html',img1=first,img2=sec,img3=tri)
+
+    
+@app.route('/dash')
+def dash():
+    '''
+        a[0] : carname
+    a[1]: price
+    a[2]: transmission
+    a[3] : fule
+    '''
+
+    cur = mysql.connection.cursor()
+    cur.execute('select * from car')
+    cars = cur.fetchall() 
+    mysql.connection.commit()
+    cur.close()
+    return render_template('base/customerdash.html',cars=cars)
+
+@app.route('/modify')
+def mod():
+    
+    cur = mysql.connection.cursor()
+    cur.execute('select * from car')
+    cars = cur.fetchall() 
+    mysql.connection.commit()
+    cur.close()
+    return render_template("base/modify.html",cars=cars)
